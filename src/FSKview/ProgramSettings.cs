6b68d1b8870e4269d65c0acd75a22460d44e3424
Scott W Harden <swharden@gmail.com>
2020-07-03 19:18:39 -0400

    create program settings module #3

---
+++ src/FSKview/ProgramSettings.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace FSKview
+{
+    public class ProgramSettings
+    {
+        public string Version;
+
+        public int audioDeviceIndex = 0;
+        public string window = "Cosine";
+        public string colormap = "Viridis";
+        public int dialFrequencyIndex = 5;
+        public double brightness = 2.0;
+        public bool isWsprEnabled = true;
+        public string wsprLogFilePath = null;
+        public bool showBands = true;
+        public bool saveGrabs = true;
+        public bool showScaleOnAllGrabs = false;
+        public string grabFileName = "latest.png";
+
+        public int targetWidth = 1000;
+        public int grabSavePxAbove = 123;
+        public int grabSavePxBelow = 321;
+        public string grabMessage = "station information not set";
+
+        public string ftpServerAddress = "ftp://ftp.qsl.net";
+        public string ftpRemoteSubfolder = "/";
+        public string ftpUsername = "sampleUsername";
+        public string ftpPassword = "do not manually edit passwords in this file";
+        public string ftpPasswordObfuscated = "c2FtcGxlUGFzc3dvcmQ";
+        public int ftpDelaySec = 15;
+
+        public ProgramSettings()
+        {
+            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            Version = $"{version.Major}.{version.Minor}";
+        }
+
+        public static ProgramSettings Load(string filePath = "settings.xml")
+        {
+            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            string expectedVersion = $"{version.Major}.{version.Minor}";
+
+            XmlSerializer reader = new XmlSerializer(typeof(ProgramSettings));
+            using (StreamReader file = new StreamReader(filePath))
+            {
+                ProgramSettings loadedConfig = (ProgramSettings)reader.Deserialize(file);
+                if (loadedConfig.Version == expectedVersion)
+                    return loadedConfig;
+                else
+                    throw new InvalidOperationException("incompatible config file version");
+            }
+        }
+
+        public void Save(string filePath = "settings.xml")
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(ProgramSettings));
+            using (StreamWriter fileWriter = new StreamWriter(filePath))
+            {
+                serializer.Serialize(fileWriter, this);
+            }
+        }
+
+        public string GetXML()
+        {
+            using (StringWriter textWriter = new StringWriter())
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(this.GetType());
+                xmlSerializer.Serialize(textWriter, this);
+                return textWriter.ToString();
+            }
+        }
+    }
+}
