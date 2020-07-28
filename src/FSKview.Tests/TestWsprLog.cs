7a5f2015a7489f457f822e6a0517c449357e52c8
Scott W Harden <swharden@gmail.com>
2020-07-27 20:50:26 -0400

    add more wspr parsing tests

--- src/FSKview.Tests/TestWsprLog.cs
+++ src/FSKview.Tests/TestWsprLog.cs
@@ -39,6 +39,70 @@ namespace FSKview.Tests
             Assert.AreEqual(7_040_078, spot.frequencyHz);
             Assert.AreEqual("G6JVT", spot.callsign);
             Assert.AreEqual("IO90", spot.grid);
+            Assert.IsTrue(spot.isValid);
+        }
+
+        [Test]
+        public void Test_LogFormat_73b()
+        {
+            string line = "200712 1326   3 -27  0.6  14.097006  HB9FID JN47 37          0   306    0";
+            var spot = new WsprSpot(line);
+
+            Assert.AreEqual(2020, spot.dt.Year);
+            Assert.AreEqual(7, spot.dt.Month);
+            Assert.AreEqual(12, spot.dt.Day);
+            Assert.AreEqual(13, spot.dt.Hour);
+            Assert.AreEqual(26, spot.dt.Minute);
+            Assert.AreEqual(0, spot.dt.Second);
+            Assert.AreEqual("2007121326", spot.timestamp);
+            Assert.AreEqual(-27, spot.strength);
+            Assert.AreEqual(14.097006, spot.frequencyMHz);
+            Assert.AreEqual(14_097_006, spot.frequencyHz);
+            Assert.AreEqual("HB9FID", spot.callsign);
+            Assert.AreEqual("JN47", spot.grid);
+            Assert.IsTrue(spot.isValid);
+        }
+
+        [Test]
+        public void Test_LogFormat_73c()
+        {
+            string line = "200712 1322   1 -28 -1.1  14.097110  <PA0ANH> JO22LH 27     -1  5495   -2";
+            var spot = new WsprSpot(line);
+
+            Assert.AreEqual(2020, spot.dt.Year);
+            Assert.AreEqual(7, spot.dt.Month);
+            Assert.AreEqual(12, spot.dt.Day);
+            Assert.AreEqual(13, spot.dt.Hour);
+            Assert.AreEqual(22, spot.dt.Minute);
+            Assert.AreEqual(0, spot.dt.Second);
+            Assert.AreEqual("2007121322", spot.timestamp);
+            Assert.AreEqual(-28, spot.strength);
+            Assert.AreEqual(14.097110, spot.frequencyMHz);
+            Assert.AreEqual(14_097_110, spot.frequencyHz);
+            Assert.AreEqual("PA0ANH", spot.callsign);
+            Assert.AreEqual("JO22LH", spot.grid);
+            Assert.IsTrue(spot.isValid);
+        }
+
+        [Test]
+        public void Test_LogFormat_73d()
+        {
+            string line = "200712 1036   5 -27 -0.5  14.097014  <...> B000AA 63         0     2    0";
+            var spot = new WsprSpot(line);
+
+            Assert.AreEqual(2020, spot.dt.Year);
+            Assert.AreEqual(7, spot.dt.Month);
+            Assert.AreEqual(12, spot.dt.Day);
+            Assert.AreEqual(10, spot.dt.Hour);
+            Assert.AreEqual(36, spot.dt.Minute);
+            Assert.AreEqual(0, spot.dt.Second);
+            Assert.AreEqual("2007121036", spot.timestamp);
+            Assert.AreEqual(-27, spot.strength);
+            Assert.AreEqual(14.097014, spot.frequencyMHz);
+            Assert.AreEqual(14_097_014, spot.frequencyHz);
+            Assert.IsNull(spot.callsign);
+            Assert.AreEqual("B000AA", spot.grid);
+            Assert.IsFalse(spot.isValid);
         }
 
         [Test]
@@ -59,6 +123,7 @@ namespace FSKview.Tests
             Assert.AreEqual(14_097_066.6, spot.frequencyHz);
             Assert.AreEqual("DL6NL", spot.callsign);
             Assert.AreEqual("JO50", spot.grid);
+            Assert.IsTrue(spot.isValid);
         }
 
         [Test]
@@ -79,6 +144,7 @@ namespace FSKview.Tests
             Assert.AreEqual(10_140_189.1, spot.frequencyHz);
             Assert.AreEqual("N8XEF", spot.callsign);
             Assert.AreEqual("EM99", spot.grid);
+            Assert.IsTrue(spot.isValid);
         }
 
         [Test]
@@ -99,6 +165,25 @@ namespace FSKview.Tests
             Assert.AreEqual(14_080_000, spot.frequencyHz);
             Assert.AreEqual("UA4CCH", spot.callsign);
             Assert.AreEqual("YO9NC", spot.grid);
+            Assert.IsTrue(spot.isValid);
+        }
+
+        [Test]
+        public void Test_LogFormat_garbage()
+        {
+            string line = "sdaf jldafj sdjfjas jfej alsjflk jes";
+            var spot = new WsprSpot(line);
+
+            Assert.IsFalse(spot.isValid);
+        }
+
+        [Test]
+        public void Test_LogFormat_empty()
+        {
+            string line = "";
+            var spot = new WsprSpot(line);
+
+            Assert.IsFalse(spot.isValid);
         }
     }
 }
