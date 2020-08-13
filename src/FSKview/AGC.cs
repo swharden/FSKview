e364feb51e04e3f9fc959289e508fa031daef2e0
Scott W Harden <swharden@gmail.com>
2020-08-12 22:28:09 -0400

    improve AGC symmetry
    #15

--- src/FSKview/AGC.cs
+++ src/FSKview/AGC.cs
@@ -8,22 +8,25 @@ namespace FSKview
 {
     public static class AGC
     {
-        public static double[] SubtractMovingWindowFloor(double[] input, int windowSizePx = 20, double percentile = .2)
+        public static double[] SubtractMovingWindowFloor(double[] input, int windowSizePx = 10, double percentile = .2)
         {
             // return a copy of the input with the noise floor subtracted
             // where the noise floor is calculated from a moving window
             // this is good but very slow
 
             double[] normalized = new double[input.Length];
-            int floorIndex = (int)(percentile * windowSizePx);
 
-            double[] segment = new double[windowSizePx];
-            for (int i = 0; i < input.Length - windowSizePx; i++)
+            double[] segment = new double[windowSizePx * 2 + 1];
+            for (int i = windowSizePx; i < input.Length - windowSizePx; i++)
             {
-                for (int j = 0; j < windowSizePx; j++)
-                    segment[j] = input[i + j];
+                for (int j = 0; j < segment.Length; j++)
+                    segment[j] = input[i + j - windowSizePx];
+
                 Array.Sort(segment);
-                normalized[i] = Math.Max(input[i] - segment[floorIndex], 0);
+
+                int floorIndex = (int)(percentile * windowSizePx);
+                double noiseFloor = segment[floorIndex];
+                normalized[i] = Math.Max(input[i] - noiseFloor, 0);
             }
 
             return normalized;
