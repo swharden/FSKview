061d611d9fcc9b1f726e5bcbf1368e5a0fd18f1f
Scott W Harden <swharden@gmail.com>
2020-08-13 20:33:42 -0400

    improve noise floor calculation
    now the mean of the lowest 20% of values in a ~20px vertical span (rather than the percentile itself) #15

--- src/FSKview/AGC.cs
+++ src/FSKview/AGC.cs
@@ -11,12 +11,14 @@ namespace FSKview
         public static double[] SubtractMovingWindowFloor(double[] input, int windowSizePx = 10, double percentile = .2, double power = 1)
         {
             // return a copy of the input with the noise floor subtracted
-            // where the noise floor is calculated from a moving window
-            // this is good but very slow
+            // where the noise floor is calculated as the mean of the quietest 20% of values in a moving window
+            // this method works but is very slow so don't use it on very fast spectrograms
 
             double[] normalized = new double[input.Length];
 
             double[] segment = new double[windowSizePx * 2 + 1];
+            int valuesInFloor = (int)(percentile * segment.Length);
+
             for (int i = windowSizePx; i < input.Length - windowSizePx; i++)
             {
                 for (int j = 0; j < segment.Length; j++)
@@ -24,9 +26,8 @@ namespace FSKview
 
                 Array.Sort(segment);
 
-                int floorIndex = (int)(percentile * windowSizePx);
-                double noiseFloor = segment[floorIndex];
-                normalized[i] = Math.Max(input[i] - noiseFloor * power, 0);
+                double meanFloor = segment.Take(valuesInFloor).Sum() / valuesInFloor;
+                normalized[i] = Math.Max(input[i] - meanFloor * power, 0);
             }
 
             return normalized;
