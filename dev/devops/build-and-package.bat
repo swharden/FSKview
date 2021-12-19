882aa632e4a4eee8e2cd1021debbeae90cd50bdc
Scott W Harden <swharden@gmail.com>
2021-12-19 12:45:14 -0500

    dev: create win7 and win10 build zip files

--- dev/devops/build-and-package.bat
+++ dev/devops/build-and-package.bat
@@ -2,7 +2,10 @@ if exist ..\..\src\FSKview\bin rmdir /s /q ..\..\src\FSKview\bin
 
 dotnet build --configuration Release ..\..\src\FSKview
 
-powershell "Compress-Archive ..\..\src\FSKview\bin\Release\net6.0-windows FSKview-win10-VERSION.zip"
-powershell "Compress-Archive ..\..\src\FSKview\bin\Release\net48 FSKview-win7-VERSION.zip"
+move ..\..\src\FSKview\bin\Release\net6.0-windows ..\..\src\FSKview\bin\Release\FSKview-win10
+move ..\..\src\FSKview\bin\Release\net48 ..\..\src\FSKview\bin\Release\FSKview-win7
+
+powershell "Compress-Archive -Force ..\..\src\FSKview\bin\Release\FSKview-win10 FSKview-VERSION-win10.zip"
+powershell "Compress-Archive -Force ..\..\src\FSKview\bin\Release\FSKview-win7 FSKview-VERSION-win7.zip"
 
 pause
