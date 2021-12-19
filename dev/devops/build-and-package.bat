b4ae10b6499e8c07dc5ece0f02e45d777ebf9355
Scott W Harden <swharden@gmail.com>
2021-12-19 12:06:17 -0500

    dev: package net48

--- dev/devops/build-and-package.bat
+++ dev/devops/build-and-package.bat
@@ -2,6 +2,7 @@ if exist ..\..\src\FSKview\bin rmdir /s /q ..\..\src\FSKview\bin
 
 dotnet build --configuration Release ..\..\src\FSKview
 
-powershell "Compress-Archive ..\..\src\FSKview\bin\Release\net6.0-windows FSKview-VERSION.zip"
+powershell "Compress-Archive ..\..\src\FSKview\bin\Release\net6.0-windows FSKview-win10-VERSION.zip"
+powershell "Compress-Archive ..\..\src\FSKview\bin\Release\net48 FSKview-win7-VERSION.zip"
 
 pause
